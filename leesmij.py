1b694ebeb48697eda62fd354abeb4f9d6a870b77
Mariska <dhondt98@hotmail.com>
2017-06-01 13:25:55 +0200

    Ja

--- leesmij.py
+++ leesmij.py
@@ -7,7 +7,7 @@ This is a temporary script file.
 
 print "is het Rens gelukt: Ja!"
 print "is het Jelle gelukt: Nog niet"
-print "is het Mariska gelukt: Nog niet"
+print "is het Mariska gelukt: Ja!"
 print "is het Julie gelukt: Nog niet"
 print "is het Joost gelukt: Nog niet"
 print "is het Eva gelukt: Nog niet"
