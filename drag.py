6c3485ecf39408cae685b450e6cd99c67da4fcbf
RogierVossebeld <rvossebeld@outlook.com>
2017-06-03 17:13:52 +0200

    clearer drag matrix
    Also rough estimate added to pilot drag in sideslip

--- drag.py
+++ drag.py
@@ -15,7 +15,11 @@ from parameters import Wing_S
 #Pilot, uit midterm report
 DA = 0.1978 #m2
 CD0_pilot = DA/Wing_S
-
+sideslip = 45 #deg
+DA_pilot_sideslip = sideslip*(0.235-0.226)/10+0.226 #lin relation assumed (see p. 32 Kilkenny)
+#print DAPilotfn
+CD0_pilot = DA
+CD0_pilot_sideslip = DA_pilot_sideslip/Wing_S
 
 #CD0 wing <-- gaat veel veranderen door airfoil design
 CD0_wing = 0.0060 #placeholder, 
@@ -26,8 +30,10 @@ wheels = 2.             #-      #amount of wheels
 LG_d  = 0.2             #m      #landing wheel diameter, placeholder value
 LG_w  = 0.1             #m      #landing wheel width, placeholder value
 LG_S  = LG_d * LG_w     #m2     #frontal area LG
+LG_S_sideslip45 = LG_d*LG_w*np.cos(45)*LG_d*np.sin(45)
 CD_LG = 0.30            #-      #is 0.15 if we include fairings
 CD0_LG = wheels*CD_LG*(LG_S/Wing_S)
+CD0_LG_sideslip45 = wheels*CD_LG*(LG_S_sideslip45/Wing_S) # CD_LG assumed to be constant, new value t.b.d.
 
 
 #LG Struts
@@ -58,9 +64,9 @@ drag_total_cruise = np.array([["Source","CD0","%of total"],
                    ["Total",CD0_total_cruise,100]
 ])
 drag_total_sideslip45 = np.array([["Source","CD0","%of total"],
-                   ["Pilot",CD0_pilot,100*CD0_pilot/CD0_total_sideslip45],
-                   ["Wing",CD0_wing,100*CD0_wing/CD0_total_sideslip45],
-                   ["LG",CD0_LG,100*CD0_LG/CD0_total_sideslip45],
+                   ["Pilot",CD0_pilot_sideslip,100*CD0_pilot_sideslip/CD0_total_sideslip45],      
+                   ["Wing",CD0_wing,100*CD0_wing/CD0_total_sideslip45],         #wing CD0 for now assumed to be constant in sideslip
+                   ["LG",CD0_LG_sideslip45,100*CD0_LG_sideslip45/CD0_total_sideslip45],
                    ["LG struts",CD0_LGstruts_sideslip45,100*CD0_LGstruts_sideslip45/CD0_total_sideslip45],
                    ["Total",CD0_total_sideslip45,100]
 ])
@@ -73,7 +79,10 @@ drag=np.zeros((5,3)) #this is going to be the float array to include all drag co
 #currently: column 0 clean, column 1 cruise, column 2 sideslip at 45deg
 #drag[[1,2,3,4,5],[1,2]]=
 dclean = np.array(clean[1:,1], dtype=float)
-drag[0:3,0] = dclean
+print drag[0:2,0]
+print dclean[0:2]
+drag[0:2,0] = dclean[0:2]
+drag[4,0]=dclean[2]
 dcruise = np.array(drag_total_cruise[1:,1], dtype=float)
 drag[0:5,1] = dcruise
 dsideslip45 = np.array(drag_total_sideslip45[1:,1], dtype=float)
